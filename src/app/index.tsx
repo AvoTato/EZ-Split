be5006da159986e760099befb350cdeec7c877e9
Iskandar Zulqarnain bin Zahari <iskandarzq03@gmail.com>
2026-09-10 15:42:44 +0800

    deleted import

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,6 @@ import { AnimatedIcon } from '@/components/animated-icon';
 import { HintRow } from '@/components/hint-row';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
 import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
 function getDevMenuHint() {
